fa0e14b6ea3cabc5dfb87257ea0cedb1b2de2026
Gabriel Harris <gch910@gmail.com>
2021-04-10 18:40:43 -0500

    follows mostly working, still need to debug

--- react-app/src/components/User/index.js
+++ react-app/src/components/User/index.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams, Redirect } from "react-router-dom";
 import UserVideos from "../UserVideos";
 import UserUploads from "../UserUploads";
 import UserFollowers from "../UserFollowers";
 import UserFollowing from "../UserFollowing";
+import { addUserFollow, getUserFollows } from "../../store/follows";
+import Button from "@material-ui/core/Button";
 import "./User.css";
 
 function User() {
+  const dispatch = useDispatch();
   const [user, setUser] = useState({});
   const { userId } = useParams();
   const sessionUser = useSelector((state) => state.session.user);
@@ -15,25 +18,35 @@ function User() {
   const [uploadedClicked, setUploadedClicked] = useState(false);
   const [followersClicked, setFollowersClicked] = useState(false);
   const [followingClicked, setFollowingClicked] = useState(false);
+  const [isFollowing, setIsFollowing] = useState(user?.is_following);
 
-  
+  console.log(user.is_following);
 
-  console.log(user)
+  const addFollow = () => {
+    dispatch(addUserFollow(user.id)).then((res) => {
+      console.log(res.result);
+      if (res.result === "follow") {
+        setIsFollowing(true);
+      } else {
+        setIsFollowing(false);
+      }
+    });
+  };
 
   const gifKeyCreator = (path) => {
-    const pathName = path.split("/")[2]
- 
-    const gifKey = pathName?.slice(0, pathName.indexOf("."))
-    
-    return gifKey
-  }
+    const pathName = path.split("/")[2];
+
+    const gifKey = pathName?.slice(0, pathName.indexOf("."));
+
+    return gifKey;
+  };
   const imgKeyCreator = (path) => {
-    const pathName = path.split("/")[2]
- 
-    const imgKey = pathName?.slice(0, pathName.indexOf("."))
-    
-    return imgKey
-  }
+    const pathName = path.split("/")[2];
+
+    const imgKey = pathName?.slice(0, pathName.indexOf("."));
+
+    return imgKey;
+  };
 
   const displayCollection = () => {
     setFollowersClicked(false);
@@ -69,42 +82,58 @@ function User() {
       const user = await response.json();
       setUser(user);
     })();
-  }, [userId, followersClicked]);
+    setIsFollowing(user.is_following)
+    // dispatch(getUserFollows(user.id));
+  }, [userId, user.is_following, followingClicked]);
 
   if (!userId) {
-    return <Redirect to="/login"/>;
+    return <Redirect to="/login" />;
   }
 
   return (
     <div>
-      <h1 id="user-favorites-h1">
-        {collectionClicked
-          ? sessionUser.id === user.id
-            ? "Your Collection"
-            : `${user.username}'s Collection`
-          : ""}
-      </h1>
-      <h1 id="user-favorites-h1">
-        {uploadedClicked
-          ? sessionUser.id === user.id
-            ? "Your Uploads"
-            : `${user.username}'s Uploads`
-          : ""}
-      </h1>
-      <h1 id="user-favorites-h1">
-        {followersClicked
-          ? sessionUser.id === user.id
-            ? "Your Followers"
-            : `${user.username}'s Followers`
-          : ""}
-      </h1>
-      <h1 id="user-favorites-h1">
-        {followingClicked
-          ? sessionUser.id === user.id
-            ? "Users You Follow"
-            : ` Users ${user.username} Follows`
-          : ""}
-      </h1>
+      <div id="profile-header">
+        <div id="collection-header">
+          {!(sessionUser.id === user.id) ?
+            <Button
+              id="follow-profile-button"
+              variant="contained"
+              onClick={() => addFollow()}
+            >
+              {isFollowing ? "Unfollow" : `Follow`}
+             
+            </Button> : ""
+          }
+          <h1 id="user-favorites-h1">
+            {collectionClicked
+              ? sessionUser.id === user.id
+                ? "Your Collection"
+                : `${user.username}'s Collection`
+              : ""}
+          </h1>
+        </div>
+        <h1 id="user-favorites-h1">
+          {uploadedClicked
+            ? sessionUser.id === user.id
+              ? "Your Uploads"
+              : `${user.username}'s Uploads`
+            : ""}
+        </h1>
+        <h1 id="user-favorites-h1">
+          {followersClicked
+            ? sessionUser.id === user.id
+              ? "Your Followers"
+              : `${user.username}'s Followers`
+            : ""}
+        </h1>
+        <h1 id="user-favorites-h1">
+          {followingClicked
+            ? sessionUser.id === user.id
+              ? "Users You Follow"
+              : ` Users ${user.username} Follows`
+            : ""}
+        </h1>
+      </div>
       <nav id="profile-nav">
         <button
           className={`profile-nav-link no-outline ${
@@ -141,16 +170,55 @@ function User() {
       </nav>
       <div id="profile-display">
         <div id="profile-songs-div">
-          {collectionClicked ? <UserVideos userId={userId} gifKeyCreator={gifKeyCreator} imgKeyCreator={imgKeyCreator} /> : ""}
+          {collectionClicked ? (
+            <UserVideos
+              userId={userId}
+              gifKeyCreator={gifKeyCreator}
+              imgKeyCreator={imgKeyCreator}
+            />
+          ) : (
+            ""
+          )}
         </div>
         <div id="profile-popular-div">
-          {uploadedClicked ? <UserUploads userId={userId} sessionUser={sessionUser} user={user} gifKeyCreator={gifKeyCreator} imgKeyCreator={imgKeyCreator} /> : ""}
+          {uploadedClicked ? (
+            <UserUploads
+              userId={userId}
+              sessionUser={sessionUser}
+              user={user}
+              gifKeyCreator={gifKeyCreator}
+              imgKeyCreator={imgKeyCreator}
+            />
+          ) : (
+            ""
+          )}
         </div>
         <div id="profile-followers-div">
-          {followersClicked ? <UserFollowers userId={userId} displayCollection={displayCollection} user={user} /> : ""}
+          {followersClicked ? (
+            <UserFollowers
+              userId={userId}
+              displayCollection={displayCollection}
+              user={user}
+            />
+          ) : (
+            ""
+          )}
         </div>
         <div id="profile-following-div">
-          {followingClicked ? <UserFollowing userId={userId} followingClicked={followingClicked} setFollowingClicked={setFollowingClicked} displayFollowers={displayFollowers} displayCollection={displayCollection} displayFollowing={displayFollowing} user={user} /> : ""}
+          {followingClicked ? (
+            <UserFollowing
+              userId={userId}
+              followingClicked={followingClicked}
+              setFollowingClicked={setFollowingClicked}
+              displayFollowers={displayFollowers}
+              displayCollection={displayCollection}
+              displayFollowing={displayFollowing}
+              sessionUser={sessionUser}
+              user={user}
+            />
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </div>
--- react-app/src/components/UserFollowing/index.js
+++ react-app/src/components/UserFollowing/index.js
@@ -1,56 +1,70 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
-import FollowButton from "../FollowButton";
 import { addUserFollow } from "../../store/follows";
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
 import "./UserFollowing.css";
 
-const UserFollowing = ({ user, displayCollection, displayFollowers, displayFollowing, followingClicked, setFollowingClicked }) => {
+const UserFollowing = ({
+  user,
+  displayCollection,
+  displayFollowers,
+  displayFollowing,
+  followingClicked,
+  setFollowingClicked,
+  sessionUser,
+}) => {
   const [isFollowing, setIsFollowing] = useState(null);
   const dispatch = useDispatch();
 
   const unfollow = (following) => {
     console.log(following);
-    dispatch(addUserFollow(following.id)).then((res) => {
-      console.log(res.result);
-      if (res.result === "follow") {
-        setIsFollowing(true);
-      } else {
-        setIsFollowing(false);
-      }
-    }).then(displayFollowers())
-    setTimeout(()=> {
-      displayFollowing()
-    }, 1)
-      
+    dispatch(addUserFollow(following.id))
+      .then((res) => {
+        console.log(res.result);
+        if (res.result === "follow") {
+          setIsFollowing(true);
+        } else {
+          setIsFollowing(false);
+        }
+      })
+      setFollowingClicked(false)
+      setTimeout(()=> {
+        setFollowingClicked(true)
+      }, 1)
   };
 
-  useEffect(() => {}, []);
+  // useEffect(() => {}, [user.following]);
 
   return (
     <div id="user-following-div">
-      {console.log(user)}
       {user.following.map((following) => (
-        <div id="following-div">
-          <NavLink to={`/users/${following.id}`} onClick={displayCollection}>
-            <img src={following?.profile_img} className="following-img" />
-          </NavLink>
-          <NavLink
-            id="following-text"
-            to={`/users/${following.id}`}
-            onClick={displayCollection}
-          >
-            {following.username}
-          </NavLink>
-          <Button
-            id="follow-button"
-            variant="contained"
-            onClick={() => unfollow(following)}
-          >
-            Unfollow
-          </Button>
-        </div>
+        <>
+          {following.id !== sessionUser.id && (
+            <div id="following-div">
+              <NavLink
+                to={`/users/${following.id}`}
+                onClick={displayCollection}
+              >
+                <img src={following?.profile_img} className="following-img" />
+              </NavLink>
+              <NavLink
+                id="following-text"
+                to={`/users/${following.id}`}
+                onClick={displayCollection}
+              >
+                {following.username}
+              </NavLink>
+              <Button
+                id="follow-button"
+                variant="contained"
+                onClick={() => unfollow(following)}
+              >
+                Unfollow
+              </Button>
+            </div>
+          )}
+        </>
       ))}
     </div>
   );
