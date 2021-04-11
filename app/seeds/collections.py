674d80ba6c62b2fa0bcdbd5b2037b30bbbd2d536
Gabriel Harris <gch910@gmail.com>
2021-04-11 15:41:01 -0500

    fixed db imports

--- app/seeds/collections.py
+++ app/seeds/collections.py
@@ -1,17 +1,21 @@
-from .users import demo1, demo2
-from .videos import user1_collection, user2_collection
+from .users import demo, user1, user2
+from .videos import user1_collection, user2_collection, user3_collection
 from app.models import db, Video, User
 
 
 def seed_collection():
     for i in range(len(user1_collection)):
-        user1 = demo1
+        user1 = demo
         user1.video_collection.append(user1_collection[i])
         db.session.add(user1)
     for i in range(len(user2_collection)):
-        user2 = demo2
+        user2 = user1
         user2.video_collection.append(user2_collection[i])
         db.session.add(user2)
+    for i in range(len(user3_collection)):
+        user3 = user2
+        user2.video_collection.append(user3_collection[i])
+        db.session.add(user3)
     
     db.session.commit()
         
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -8,14 +8,14 @@ user1 = User(username="User1", email='user1@aa.io', password='password')
 
 user2 = User(username="User2", email='user2@aa.io', password='password')
 
-user3 = User(username="User3", email='user3@aa.io', password='password')
+# user3 = User(username="User3", email='user3@aa.io', password='password')
 
 
 def seed_users():
     db.session.add(demo)
     db.session.add(user1)
     db.session.add(user2)
-    db.session.add(user3)
+    # db.session.add(user3)
     db.session.commit()
 
 # Uses a raw SQL query to TRUNCATE the users table.
