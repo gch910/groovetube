9e532f4cf14e1f9d2389e8cbcf71af0949bded17
Gabriel Harris <gch910@gmail.com>
2021-07-05 12:48:44 -0500

    key creators imported for search results

--- react-app/src/components/Nav/SearchResults.js
+++ react-app/src/components/Nav/SearchResults.js
@@ -2,35 +2,11 @@ import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import "./SearchResults.css";
-import gifs from "../Home/gifs";
 import imgs from "../Home/images";
+import { imgKeyCreator, changeImg } from "../utils";
 
 const SearchResults = () => {
   const searchResults = useSelector((state) => state.videos.search_results);
-
-  const changeImg = (e, video) => {
-    if(video && gifs[gifKeyCreator(video.gif_path)]) {
-      e.target.src = gifs[gifKeyCreator(video.gif_path)]
-    } else {
-      video && (e.target.src = "https://i.stack.imgur.com/y9DpT.jpg");
-    }
-    
-  };
-
-  const gifKeyCreator = (path) => {
-    const pathName = path.split("/")[2]
- 
-    const gifKey = pathName?.slice(0, pathName.indexOf("."))
-    
-    return gifKey
-  }
-  const imgKeyCreator = (path) => {
-    const pathName = path.split("/")[2]
- 
-    const imgKey = pathName?.slice(0, pathName.indexOf("."))
-    
-    return imgKey
-  }
 
   return (
     <div id="outer-search-results">
@@ -44,11 +20,19 @@ const SearchResults = () => {
           <div id="search-results-div">
             <Link to={`/videos/${video.id}`}>
               <img
-                 onMouseEnter={(e) => changeImg(e, video)}
-                 onMouseLeave={(e) => imgs[imgKeyCreator(video.img_path)] ? e.target.src = imgs[imgKeyCreator(video.img_path)] : e.target.src = video.img_path}
+                onMouseEnter={(e) => changeImg(e, video)}
+                onMouseLeave={(e) =>
+                  imgs[imgKeyCreator(video.img_path)]
+                    ? (e.target.src = imgs[imgKeyCreator(video.img_path)])
+                    : (e.target.src = video.img_path)
+                }
                 className="thumbnail"
                 id="search-img"
-                src={imgs[imgKeyCreator(video.img_path)] ? imgs[imgKeyCreator(video.img_path)] : video.img_path}
+                src={
+                  imgs[imgKeyCreator(video.img_path)]
+                    ? imgs[imgKeyCreator(video.img_path)]
+                    : video.img_path
+                }
               />
             </Link>
           </div>
