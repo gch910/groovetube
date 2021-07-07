ad36d6dae292d8109a0b3d9725a419e8f6944398
Gabriel Harris <gch910@gmail.com>
2021-07-07 12:38:38 -0500

    comment added to UserHeader

--- react-app/src/components/User/UserHeader.js
+++ react-app/src/components/User/UserHeader.js
@@ -3,6 +3,7 @@ import { useHistory } from "react-router-dom";
 
 export const UserHeader = ({ user, text }) => {
   const history = useHistory();
+  //different change image function from what is used for video gifs, so we do not import from utils.js
   const changeImg = (e) => {
     e.target.src = "https://static.thenounproject.com/png/396915-200.png";
   };
